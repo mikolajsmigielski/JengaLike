78a55fa20037e7bff5854988662fbdc88db40c81
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-06-29 15:28:48 +0200

    Create level and camera movment

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    float AxisX;
+    float AxisY;
+
+    public float distance = 25f;
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Mouse X");
+        float deltaY = Input.GetAxis("Mouse Y");
+
+        AxisX += deltaY * speed;
+        AxisY += deltaX * speed;
+        var rotation = Quaternion.Euler(AxisX, AxisY, 0);
+        transform.position = rotation * Vector3.up * distance;
+
+        transform.LookAt(Vector3.zero);
+    }
+}
